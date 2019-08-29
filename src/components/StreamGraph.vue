d5089e8be921636e171221da8d461d3875a557df
Ian Hunter <ijh.hunter@gmail.com>
2019-08-29 00:39:54 -0400

    moved graph into its own component

---
+++ src/components/StreamGraph.vue
@@ -0,0 +1,191 @@
+<template>
+  <v-card>
+    <v-toolbar
+      flat
+      dense
+    >
+      <v-toolbar-title>
+        {{ stream.title }}
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-sparkline
+      :gradient="gradient"
+      :line-width="2"
+      :smooth="radius || false"
+      :value="graphValues"
+      auto-draw
+    ></v-sparkline>
+
+    <v-list>
+      <v-list-item>
+        <v-list-item-title>Joke Score</v-list-item-title>
+
+        <v-list-item-subtitle class="text-right">
+          {{ total }}
+        </v-list-item-subtitle>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-title>Min</v-list-item-title>
+
+        <v-list-item-subtitle class="text-right">
+          {{ min }}
+        </v-list-item-subtitle>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-title>Max</v-list-item-title>
+
+        <v-list-item-subtitle class="text-right">
+          {{ max }}
+        </v-list-item-subtitle>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-title>Low</v-list-item-title>
+
+        <v-list-item-subtitle class="text-right">
+          {{ low }}
+        </v-list-item-subtitle>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-title>high</v-list-item-title>
+
+        <v-list-item-subtitle class="text-right">
+          {{ high }}
+        </v-list-item-subtitle>
+      </v-list-item>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+import moment from 'moment'
+import client from '@/plugins/tmi'
+import { mapState, mapActions } from 'vuex'
+
+export default {
+  name: 'StreamGraph',
+  data () {
+    return {
+      messages: [],
+      min: 0,
+      max: 0,
+      high: 0,
+      low: 0,
+      total: 0,
+      dataPoints: [],
+      now: moment(),
+      radius: 10,
+      gradient: ['#f72047', '#ffd200', '#1feaea']
+    }
+  },
+  computed: {
+    ...mapState(['stream']),
+    graphValues () {
+      return this.dataPoints.map(data => data.jokeScore)
+    },
+    streamUpTime () {
+      if (this.stream.type === 'live') return this.now.diff(moment(this.stream.startedAt), 'minutes')
+
+      if (this.stream.streamUpTime) return this.stream.streamUpTime
+
+      return this.stream.data[this.stream.data.length - 1].interval
+    }
+  },
+  created () {
+    this.total = this.stream.jokeScoreTotal
+    this.min = this.stream.jokeScoreMin
+    this.max = this.stream.jokeScoreMax
+    this.high = this.stream.jokeScoreHigh
+    this.low = this.stream.jokeScoreLow
+
+    this.dataPoints = this.calcDataPoints()
+
+    if (this.stream.type === 'live') {
+      console.log('Stream is live')
+      setInterval(this.updateGraph, 5000)
+      client.on('message', this.onMessageHandler)
+    }
+  },
+  methods: {
+    ...mapActions(['fetchStream']),
+    onMessageHandler (channel, userstate, message, self) {
+      if (message.includes('+2')) {
+        userstate.joke = true
+        userstate.message = message
+        this.messages.push(userstate)
+      } else if (message.includes('-2')) {
+        userstate.joke = false
+        userstate.message = message
+        this.messages.push(userstate)
+      }
+    },
+    calcDataPoints () {
+      const data = this.stream.data.slice().reverse()
+      const dataPoints = []
+      let jokeScore = 0
+      for (let i = 0; i <= this.streamUpTime; i++) {
+        const dataPoint = data.find(data => data.interval === i)
+        if (dataPoint) {
+          jokeScore = dataPoint.jokeScore
+          dataPoints.push(dataPoint)
+        } else {
+          dataPoints.push({
+            jokeScore: jokeScore,
+            interval: i
+          })
+        }
+      }
+
+      return dataPoints
+    },
+    analyseMessages () {
+      this.total = this.messages.reduce((sum, message) => {
+        return message.joke ? sum + 2 : sum - 2
+      }, this.stream.jokeScoreTotal)
+
+      this.min = this.messages.reduce((sum, message) => {
+        return message.joke ? sum : sum - 2
+      }, this.stream.jokeScoreMin)
+
+      this.max = this.messages.reduce((sum, message) => {
+        return message.joke ? sum + 2 : sum
+      }, this.stream.jokeScoreMax)
+
+      this.messages.reduce((sum, message) => {
+        message.joke ? sum += 2 : sum -= 2
+        if (sum > this.high) this.high = sum
+        return sum
+      }, this.stream.jokeScoreTotal)
+
+      this.messages.reduce((sum, message) => {
+        message.joke ? sum += 2 : sum -= 2
+        if (sum < this.low) this.low = sum
+        return sum
+      }, this.stream.jokeScoreTotal)
+    },
+    updateGraph () {
+      this.now = moment()
+      this.analyseMessages()
+
+      const dataPointIndex = this.dataPoints.findIndex(data => data.interval === this.streamUpTime)
+
+      if (dataPointIndex !== -1) {
+        this.dataPoints[dataPointIndex].jokeScore = this.total
+      } else {
+        this.dataPoints.push({
+          jokeScore: this.total,
+          interval: this.streamUpTime
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/StreamDetails.vue
+++ src/views/StreamDetails.vue
@@ -1,79 +1,20 @@
 <template>
   <v-container>
     <v-row>
+      <v-col v-if="stream">
+        <StreamGraph />
+      </v-col>
+    </v-row>
+
+    <v-row>
       <v-col>
-        <v-card :loading="loading">
-          <template v-if="!loading">
-            <v-toolbar
-              flat
-              dense
-            >
-              <v-toolbar-title>
-                {{ stream.title }}
-              </v-toolbar-title>
-            </v-toolbar>
 
-            <v-sparkline
-              :gradient="gradient"
-              :line-width="2"
-              :smooth="radius || false"
-              :value="graphValues"
-              auto-draw
-            ></v-sparkline>
-
-            <v-list>
-              <v-list-item>
-                <v-list-item-title>Joke Score</v-list-item-title>
-
-                <v-list-item-subtitle class="text-right">
-                  {{ total }}
-                </v-list-item-subtitle>
-              </v-list-item>
-
-              <v-list-item>
-                <v-list-item-title>Min</v-list-item-title>
-
-                <v-list-item-subtitle class="text-right">
-                  {{ min }}
-                </v-list-item-subtitle>
-              </v-list-item>
-
-              <v-list-item>
-                <v-list-item-title>Max</v-list-item-title>
-
-                <v-list-item-subtitle class="text-right">
-                  {{ max }}
-                </v-list-item-subtitle>
-              </v-list-item>
-
-              <v-list-item>
-                <v-list-item-title>Low</v-list-item-title>
-
-                <v-list-item-subtitle class="text-right">
-                  {{ low }}
-                </v-list-item-subtitle>
-              </v-list-item>
-
-              <v-list-item>
-                <v-list-item-title>high</v-list-item-title>
-
-                <v-list-item-subtitle class="text-right">
-                  {{ high }}
-                </v-list-item-subtitle>
-              </v-list-item>
-            </v-list>
-          </template>
-        </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import moment from 'moment'
-
-import client from '@/plugins/tmi'
-
 import { mapState, mapActions } from 'vuex'
 
 export default {
@@ -84,131 +25,25 @@ export default {
       required: true
     }
   },
+  components: {
+    StreamGraph: () => import('@/components/StreamGraph')
+  },
   data () {
     return {
-      loading: true,
-      radius: 10,
-      gradient: ['#f72047', '#ffd200', '#1feaea'],
-      messages: [],
-      min: 0,
-      max: 0,
-      high: 0,
-      low: 0,
-      total: 0,
-      dataPoints: [],
-      updateInterval: null,
-      now: moment(),
-      title: ''
     }
   },
   computed: {
-    ...mapState(['stream', 'game']),
-    graphValues () {
-      return this.dataPoints.map(data => data.jokeScore)
-    },
-    streamUpTime () {
-      if (this.stream.type === 'live') return this.now.diff(moment(this.stream.startedAt), 'minutes')
-
-      if (this.stream.streamUpTime) return this.stream.streamUpTime
-
-      return this.stream.data[this.stream.data.length - 1].interval
-    }
+    ...mapState(['stream'])
   },
   async created () {
     try {
-      this.loading = true
       await this.fetchStream(this.streamID)
-      await this.fetchGame(this.stream.gameID)
-      this.total = this.stream.jokeScoreTotal
-      this.min = this.stream.jokeScoreMin
-      this.max = this.stream.jokeScoreMax
-      this.high = this.stream.jokeScoreHigh
-      this.low = this.stream.jokeScoreLow
-      this.dataPoints = this.calcDataPoints()
-      if (this.stream.type === 'live') {
-        console.log('Stream is live')
-        setInterval(this.updateGraph, 5000)
-        client.on('message', this.onMessageHandler)
-      }
     } catch (error) {
       console.error('Failed to fetch stream:', error)
-    } finally {
-      this.loading = false
     }
   },
   methods: {
-    ...mapActions(['fetchStream', 'fetchGame']),
-    onMessageHandler (channel, userstate, message, self) {
-      if (message.includes('+2')) {
-        userstate.joke = true
-        userstate.message = message
-        this.messages.push(userstate)
-      } else if (message.includes('-2')) {
-        userstate.joke = false
-        userstate.message = message
-        this.messages.push(userstate)
-      }
-    },
-    calcDataPoints () {
-      const data = this.stream.data.slice().reverse()
-      const dataPoints = []
-      let jokeScore = 0
-      for (let i = 0; i <= this.streamUpTime; i++) {
-        const dataPoint = data.find(data => data.interval === i)
-        if (dataPoint) {
-          jokeScore = dataPoint.jokeScore
-          dataPoints.push(dataPoint)
-        } else {
-          dataPoints.push({
-            jokeScore: jokeScore,
-            interval: i
-          })
-        }
-      }
-
-      return dataPoints
-    },
-    analyseMessages () {
-      // Calculate the total joke score so far
-      this.total = this.messages.reduce((sum, message) => {
-        return message.joke ? sum + 2 : sum - 2
-      }, this.stream.jokeScoreTotal)
-
-      this.min = this.messages.reduce((sum, message) => {
-        return message.joke ? sum : sum - 2
-      }, this.stream.jokeScoreMin)
-
-      this.max = this.messages.reduce((sum, message) => {
-        return message.joke ? sum + 2 : sum
-      }, this.stream.jokeScoreMax)
-
-      this.messages.reduce((sum, message) => {
-        message.joke ? sum += 2 : sum -= 2
-        if (sum > this.high) this.high = sum
-        return sum
-      }, this.stream.jokeScoreTotal)
-
-      this.messages.reduce((sum, message) => {
-        message.joke ? sum += 2 : sum -= 2
-        if (sum < this.low) this.low = sum
-        return sum
-      }, this.stream.jokeScoreTotal)
-    },
-    updateGraph () {
-      this.now = moment()
-      this.analyseMessages()
-
-      const dataPointIndex = this.dataPoints.findIndex(data => data.interval === this.streamUpTime)
-
-      if (dataPointIndex !== -1) {
-        this.dataPoints[dataPointIndex].jokeScore = this.total
-      } else {
-        this.dataPoints.push({
-          jokeScore: this.total,
-          interval: this.streamUpTime
-        })
-      }
-    }
+    ...mapActions(['fetchStream'])
   }
 }
 </script>
