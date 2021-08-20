1382067c86dc8b1e4618c5c03ab44f8444e9d4a3
ihunter <ijh.hunter@gmail.com>
2021-08-19 20:11:33 -0400

    update packages

--- src/plugins/tmi.js
+++ src/plugins/tmi.js
@@ -2,10 +2,6 @@ import tmi from 'tmi.js'
 
 // eslint-disable-next-line new-cap
 const client = new tmi.client({
-  connection: {
-    reconnect: true,
-    secure: true
-  },
   channels: process.env.VUE_APP_CHANNEL_NAME.split(' ')
 })
 
