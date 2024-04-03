041db4a0534d0265df3fa7e34a91337276885307
ihunter <ijh.hunter@gmail.com>
2024-04-03 17:18:52 -0400

    update babel parser and fix apexchart error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,15 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
-  ],
+  extends: ["plugin:vue/essential", "@vue/standard"],
   rules: {
     // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-console': 0,
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-console": 0,
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
   },
   parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+    parser: "@babel/eslint-parser",
+  },
+};
