bf80c0ee8deda9cfdbbbee9da248bad6aca8a1f7
ihunter <ijh.hunter@gmail.com>
2025-08-09 19:54:33 -0400

    fix env vars

--- app/plugins/firebase.ts
+++ app/plugins/firebase.ts
@@ -2,15 +2,15 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 export default defineNuxtPlugin(() => {
-  const config = useRuntimeConfig()
 
   const firebaseConfig = {
-    apiKey: config.public.firebaseApiKey,
-    authDomain: config.public.firebaseAuthDomain,
-    projectId: config.public.firebaseProjectId,
-    storageBucket: config.public.firebaseStorageBucket,
-    messagingSenderId: config.public.firebaseMessagingSenderId,
-    appId: config.public.firebaseAppId,
+    apiKey: "AIzaSyAz0iwy0SUEdgfEepd58a0DPZwxKoD8MQU",
+    authDomain: "jerma-joke.firebaseapp.com",
+    databaseURL: "https://jerma-joke.firebaseio.com",
+    projectId: "jerma-joke",
+    storageBucket: "jerma-joke.appspot.com",
+    messagingSenderId: "799027726679",
+    appId: "1:799027726679:web:807cf04ad8529c83"
   }
 
   const app = initializeApp(firebaseConfig)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,13 +9,6 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      firebaseApiKey: process.env.FIREBASE_API_KEY,
-      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      firebaseDatabaseURL: process.env.FIREBASE_DATABASE_URL,
-      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      firebaseAppId: process.env.FIREBASE_APP_ID,
       twitchChannelName: process.env.TWITCH_CHANNEL_NAME,
     },
   },
